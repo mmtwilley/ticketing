d2e0ac32db4927cd5c01bf50538181b78ffce08c
Marvin Twilley <marvinmtwilley@gmail.com>
2023-08-19 14:56:47 -0500

    new startup message

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,19 +1,21 @@
-import Queue from "bull";
+import Queue from 'bull';
+import { ExpirationCompletePublisher } from '../events/publishers/expiration-complete-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
-interface Payload{
-    orderId: string;
+interface Payload {
+  orderId: string;
 }
 
-const expirationQueue = new Queue<Payload>('order:expiration',{
-    redis:{
-        host: process.env.REDIS_HOST
-    }
+const expirationQueue = new Queue<Payload>('order:expiration', {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
 });
 
-expirationQueue.process( async (job) =>{
-    console.log('I want to publish an expiration:complete even for orderId', 
-    job.data.orderId
-    );
+expirationQueue.process(async (job) => {
+  new ExpirationCompletePublisher(natsWrapper.client).publish({
+    orderId: job.data.orderId,
+  });
 });
 
-export {expirationQueue};
+export { expirationQueue };
